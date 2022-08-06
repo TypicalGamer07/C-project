382053b9cff4bf6fefcb1437912ab54a8fbfe90a
Subhabrata <70436487+TypicalGamer07@users.noreply.github.com>
2022-08-06 16:31:35 +0530

    base

--- Basic Calculator/base.c
+++ Basic Calculator/base.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
 void main () {
-    printf ("Hello");
-    getchar ();
+    printf ("Welcome to my C calculator, this is get more advanced & complicated time to time \n");
+    printf ("Give your integer input \n");
+    int total,n1,n2;
+    scanf ("%d %d",&n1,&n2);
+    total = n1 + n2;
+    printf ("%d + %d =%d \n", n1,n2,total);
+    system("pause");
 }
